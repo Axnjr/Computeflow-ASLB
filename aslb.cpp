f368796d038b60927191a203e17c82294d8d4232
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-23 06:46:33 +0530

    feat: Server

--- aslb.cpp
+++ aslb.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+//#include "rapidjson/document.h"
+#include "httplib.h"
 #include "crow.h"
+#include <iostream>
 
-int main(){
-    std::cout << "Hello World!\n";
+using namespace std;
+using namespace httplib;
+
+int main() {
+    crow::SimpleApp Server;
+
+   /* CROW_CATCHALL_ROUTE(Server)([](const crow::request& req, crow::response& res, std::string path) {
+        std::string host = req.get_header_value("Host");
+        std::string subdomain = host.substr(0, host.find('.'));
+
+        cout << "HOST:" << host << endl;
+        cout << "SUB DOMAIN:" << subdomain << endl;
+        cout << "BODY:" << req.body << endl;
+
+        cout << "PATH:" << endl;
+        for (auto i : path) {
+            cout << i << endl;
+        }
+
+        res.write("WRITTING A RESPONSE TO `write` method !");
+        res.end();
+        });*/
+
+    Server.loglevel(crow::LogLevel::Info);
+    Server.port(3000).multithreaded().run_async();
+    return 0;
 }
