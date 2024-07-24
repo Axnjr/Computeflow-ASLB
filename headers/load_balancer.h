b71193df3e740504ca402b2be3e7bddd8150bcd3
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-24 07:17:44 +0530

    refractor: fix include error

--- headers/load_balancer.h
+++ headers/load_balancer.h
@@ -1,18 +1,18 @@
-#ifndef SERVER_LOAD_BALANCER_HEADER
-#define SERVER_LOAD_BALANCER_HEADER
-
-#include "utils.h"
+#ifndef SERVER_LOAD_BALANCER_H
+#define SERVER_LOAD_BALANCER_H
 #include <vector>
+#include <iostream>
 
-static vector<string> ip_pool = get_ip_pool();
+using namespace std;
 
-string get_server_ip(static int& REQUEST_COUNT) {
-	if (REQUEST_COUNT >= ip_pool.size()) {
-		REQUEST_COUNT = 0;
-		cout << "REQUEST COUNT RESET !!" << endl;
-	}
-	cout << "REQUEST COUNT: " << REQUEST_COUNT << endl;
-	return ip_pool[REQUEST_COUNT++];
+// Sticky Session to be implmented
+string get_server_ip(int& REQUEST_COUNT, const vector<string>& ip_pool) {
+    if (REQUEST_COUNT >= ip_pool.size()) {
+        REQUEST_COUNT = 0;
+        cout << "REQUEST COUNT RESET !!" << endl;
+    }
+    cout << "REQUEST COUNT: " << REQUEST_COUNT << endl;
+    return ip_pool[REQUEST_COUNT++];
 }
 
-#endif // !SERVER_LOAD_BALANCER_HEADER
+#endif // SERVER_LOAD_BALANCER_H
