2cb0bc90ca3410b54ca9861e9d9bbbac757f7faf
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-08-01 21:50:28 +0530

    feat server utilities

---
+++ utils.h
@@ -0,0 +1,47 @@
+#include "dependencies/httplib.h"
+
+using namespace std;
+using namespace httplib;
+
+static string stringify_headers(const Headers& headers) {
+    string s;
+    char buf[BUFSIZ];
+    for (const auto& x : headers) {
+        snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
+        s += buf;
+    }
+    return s;
+}
+
+static string log(const Request& req, const Response& res) {
+    string s;
+    string query;
+    char buf[BUFSIZ];
+
+    s += "================================\n";
+
+    snprintf(buf, sizeof(buf), "%s %s %s", req.method.c_str(),
+        req.version.c_str(), req.path.c_str());
+    s += buf;
+
+    for (auto it = req.params.begin(); it != req.params.end(); ++it) {
+        const auto& x = *it;
+        snprintf(buf, sizeof(buf), "%c%s=%s",
+            (it == req.params.begin()) ? '?' : '&', x.first.c_str(),
+            x.second.c_str());
+        query += buf;
+    }
+    snprintf(buf, sizeof(buf), "%s\n", query.c_str());
+    s += buf;
+
+    s += stringify_headers(req.headers);
+    //s += dump_multipart_files(req.files); method removed / disabled
+
+    s += "--------------------------------\n";
+
+    snprintf(buf, sizeof(buf), "%d\n", res.status);
+    s += buf;
+    s += stringify_headers(res.headers);
+
+    return s;
+}
