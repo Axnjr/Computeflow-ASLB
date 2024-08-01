3311e409c67d13b276b5cf4c84bb0cb0308d371c
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-08-01 21:52:29 +0530

    moved headers out

---
+++ fetch_response.h
@@ -0,0 +1,17 @@
+﻿#ifndef FETCH_RESPONSE_FROM_ORIGINAL_SERVER_H
+#define FETCH_RESPONSE_FROM_ORIGINAL_SERVER_H
+
+#include "dependencies/httplib.h"
+
+using namespace std;
+using namespace httplib;
+
+auto fetch_response_from_ip(string ip, string req, int port){
+	cout << endl << "REQ-PATH:" << req << endl;
+	httplib::Client PROXY_CLIENT("localhost", 3000); // port to be added
+	auto res_forwarded = PROXY_CLIENT.Get(req);
+	cout << "IP:" << ip << res_forwarded.error() << endl;
+	return res_forwarded;
+}
+
+#endif // !FETCH_RESPONSE_FROM_ORIGINAL_SERVER_H
---
+++ load_config.h
@@ -0,0 +1,38 @@
+﻿#ifndef SERVER_CONFIG_LOADER_H
+#define SERVER_CONFIG_LOADER_H
+
+#include "rapidjson/document.h"
+#include <vector>
+#include <fstream> 
+#include <iostream> 
+
+using namespace std;
+using namespace rapidjson;
+
+void initialize_static_memory_from_config(int& PORT, vector<string>& IP_POOL) {
+	ifstream config_file("aslb_config.json");
+	string json((istreambuf_iterator<char>(config_file)), istreambuf_iterator<char>());
+
+	Document doc;
+	doc.Parse(json.c_str());
+
+	if (doc.HasParseError()) {
+		cerr << "Error parsing `aslb_config.json`: " << doc.GetParseError() << endl;
+		throw doc.GetParseError();
+	}
+
+	cout << endl << "⚠️👀✌️💸 DATA LOADED FROM CONFIG IN `dev` MODE: REPLACE `ipPoolDev` WITH `ipPool` 💵🤞😓" << endl;
+
+	for (const auto& ip : doc["ipPoolDev"].GetArray()) { // change to `ipPool` in production ⚠️👀
+		if (ip.IsString()) {
+			IP_POOL.push_back(ip.GetString());
+		}
+	}
+
+	PORT = doc["port"].GetInt();
+
+	cout << endl << "--------------------- CONFIG FILE WAS READ -----------------------" << endl;
+	cout << "PORT: " << PORT << endl;
+}
+
+#endif // !SERVER_CONFIG_LOADER_H
