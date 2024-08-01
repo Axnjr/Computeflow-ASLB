a956cfea98614c1e623e6650d0d711e9b87f2506
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-08-01 21:53:07 +0530

    feat:  testing client

---
+++ test_client.mjs
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.send("dfghjk")
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+io.on("JOIN", (mes) => {
+	console.log(mes)
+})
+
+io.on("PING ASLB", (mes) => {
+	console.log(mes)
+})
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
