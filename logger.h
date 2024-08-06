16bddcc015678d0026ed6b6992efe24bd502d978
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-08-06 15:59:44 +0530

    feat: maintain log file upto 10 MB only

--- logger.h
+++ logger.h
@@ -6,23 +6,36 @@
 #include <string>
 #include <ctime>
 #include <sstream>
+#include <sys/stat.h>
 
 using namespace std;
 
 namespace logger {
 
-	//void write_log(const string& message) {
-	//    ofstream logFile;
-	//    logFile.open("load_balancer.log", std::ios_base::app); // Open in append mode
-	//    if (logFile.is_open())
-	//    {
-	//        logFile << "[" << getCurrentTime() << "]" << " ~ " << message << endl;
-	//        logFile.close();
-	//    }
-	//    else {
-	//        cerr << "Unable to open log file \n";
-	//    }
-	//}
+	const std::string logFilePath = "aslb.log";
+	const size_t maxLogFileSize = 1024 * 1024 * 10; // 10 MB
+
+	void checkAndTruncateLogFile() {
+		struct stat fileStat;
+		if (stat(logFilePath.c_str(), &fileStat) == 0) 
+		{
+			if (fileStat.st_size > maxLogFileSize) 
+			{
+				std::ofstream logFile(logFilePath, std::ios::trunc); // Open file in truncation mode
+				if (logFile.is_open()) 
+				{
+					logFile.close();
+					std::cout << "Log file truncated successfully." << std::endl;
+				}
+				else {
+					std::cerr << "Failed to open log file for truncation." << std::endl;
+				}
+			}
+		}
+		else {
+			std::cerr << "Failed to get file status." << std::endl;
+		}
+	}
 
 	// Function to get the current time as a string
 	std::string getCurrentTime() {
@@ -33,15 +46,25 @@ namespace logger {
 	}
 
 	// Variadic template function to log multiple arguments
-	template <typename... Args>
-	void ltf(Args... args) {
+	template <typename... Args> void ltf(Args... args) 
+	{
+		checkAndTruncateLogFile();
+
 		std::ofstream logFile;
 		logFile.open("aslb.log", std::ios_base::app); // Open in append mode
 
 		if (logFile.is_open()) {
+
 			std::stringstream ss;
-			ss << "[" << getCurrentTime() << "]" << " ◆ ";
-			(ss << ... << args);  // Fold expression to handle variadic arguments
+
+			ss << "[" << getCurrentTime() << "]" << " ~ ";
+			// Fold expression to handle variadic arguments
+			(ss << ... << args);  
+
+		#ifdef DEV_MODE
+			cout << ss.str() << endl;
+		#endif // DEV_MODE
+
 			logFile << ss.str() << std::endl;
 			logFile.close();
 		}
