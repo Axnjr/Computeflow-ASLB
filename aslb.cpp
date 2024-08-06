d0526b8cf893561327b672bb72f94af0e86a362b
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-08-06 16:05:46 +0530

    feat: notifications, logging, status handler & small tweaks

--- aslb.cpp
+++ aslb.cpp
@@ -1,14 +1,16 @@
 ﻿#define DEV_MODE
+
 #include "dependencies/httplib.h"
 #include "ip_cache.h"
 #include "load_config.h"
-#include "fetch_response.h"
 #include "utils.h"
 #include "rapidjson/document.h"
 #include "lb_config_struct.h"
-#include "scaling.h"
+//#include "scaling.h"
 #include "logger.h"
+#include "notifications.h"
 #include <iostream>
+#include <mutex>
 
 using namespace std;
 using namespace httplib;
@@ -30,33 +32,42 @@ struct CLIENT_USAGE_DATA {
 
 ip_cache cache;
 
+static int CURRENT_IP_POOL_INDEX = 0;
+
 int ip_cache::cap = 100;
+
+static pair<int64_t, std::chrono::system_clock::time_point> REQUESTS = { 
+	0, 
+	chrono::system_clock::now() 
+};
+
 list<string> ip_cache::lru;
+
 unordered_map<string, pair<list<string>::iterator, int>> ip_cache::lru_map;
 unordered_map<string, CLIENT_USAGE_DATA> client_usage_map;
 
-static int HTTP_REQUEST_COUNT = 0;
-
 static string map_clientIp_to_serverIp(string ip) {
 
-	int r = cache.find_ip(ip, HTTP_REQUEST_COUNT);
+	int r = cache.find_ip(ip, CURRENT_IP_POOL_INDEX);
 
 	if (r != -1) {
 		return LB_CONFIG::IP_POOL[r];
 	}
 
-	else if (HTTP_REQUEST_COUNT >= LB_CONFIG::IP_POOL.size()) {
-		HTTP_REQUEST_COUNT = 0;
+	else if (CURRENT_IP_POOL_INDEX >= LB_CONFIG::IP_POOL.size()) {
+		CURRENT_IP_POOL_INDEX = 0;
 		//cout << "REQUEST COUNT RESET !!" << endl;
 	}
 
 	//cout << "REQUEST COUNT: " << HTTP_REQUEST_COUNT << endl;
-	return LB_CONFIG::IP_POOL[HTTP_REQUEST_COUNT++];
+	return LB_CONFIG::IP_POOL[CURRENT_IP_POOL_INDEX++];
 }
 
 static void request_handler(const Request& req, Response& res) {
 	string ip = map_clientIp_to_serverIp(req.remote_addr);
-	auto op = fetch_response_from_ip("", req.path, LB_CONFIG::PORT);
+	httplib::Client PROXY_CLIENT("172.27.48.1", 3000); // port to be added
+	auto op = PROXY_CLIENT.Get(req.path);
+	cout << "IP:" << ip << op.error() << endl;
 	res.body = op->body;
 	res.status = op->status;
 	res.headers = op->headers;
@@ -75,7 +86,17 @@ static void analyze_vm_state(CLIENT_USAGE_DATA usage_data) {
 	)
 	{
 		// scale vm up ..
-		thread(scale_up).detach();
+		//thread(scale_up).detach();
+		notify_via_email(
+			"Traffic Increased !!",
+
+			"The load-balancer added a VM to the fleet due to incresed traffic, at: " + 
+			timePointToString(chrono::system_clock::now()) +
+			"\n CPU UASGE: " +
+			to_string(usage_data.cpuUsage) +
+			"\n MEMORY CONSUMPTION: " + 
+			to_string(usage_data.memUsage)
+		);
 		return;
 	}
 
@@ -89,8 +110,19 @@ static void analyze_vm_state(CLIENT_USAGE_DATA usage_data) {
 		LB_CONFIG::vmCount > LB_CONFIG::minVms
 	)
 	{
+		cout << endl << usage_data.cpuUsage << " | " << usage_data.memUsage << endl;
 		// scale vm down ..
-		thread(scale_down, usage_data.ip).detach();
+		//thread(scale_down, usage_data.ip).detach();
+		notify_via_email(
+			"Traffic Decreased !!",
+
+			"The load-balancer removed a VM from the fleet due to decresed traffic, at: " +
+			timePointToString(chrono::system_clock::now()) +
+			"\n CPU UASGE: " +
+			to_string(usage_data.cpuUsage) +
+			"\n MEMORY CONSUMPTION: " +
+			to_string(usage_data.memUsage)
+		);
 		return;
 	}
 
@@ -107,20 +139,27 @@ int main(void) {
 		cout << log(req, res); 
 	});
 
+	svr.set_pre_routing_handler([](const auto& req, auto& res) {
+		REQUESTS.first++;
+	    return Server::HandlerResponse::Unhandled;
+	});
+
 	svr.Post(".*", [&](const Request& req, Response& res)
 	{
 		if (req.path == "/ping")
 		{
+			// Request count is reset every 15 mins
+			REQUESTS.first = 0;
+			REQUESTS.second = chrono::system_clock::now();
+
 			ltf("PING FROM VM: ", req.remote_addr, " STATUS: ", req.body);
 
 			JsonMessage.Parse(req.body.c_str());
-			if (JsonMessage.HasParseError())
-			{
+			if (JsonMessage.HasParseError()) {
 				ltf("Error parsing `PING REQUEST PAYLOAD`: ", JsonMessage.GetParseError());
 			}
 
 			auto client_usage_map_it = client_usage_map.find(req.remote_addr);
-
 			if (client_usage_map_it == client_usage_map.end())
 			{
 				CLIENT_USAGE_DATA cud;
@@ -140,7 +179,6 @@ int main(void) {
 			else
 			{
 				auto& temp = *client_usage_map_it;
-
 				temp.second.cpuUsage = JsonMessage["cpuUsage"].GetFloat();
 				temp.second.memUsage = JsonMessage["memoryUsage"].GetFloat();
 				temp.second.systemUptime = JsonMessage["systemUptime"].GetFloat();
@@ -152,23 +190,56 @@ int main(void) {
 			}
 			res.status = 200;
 		}
-		else if (req.path == "/ping_error") {
+
+		else if (req.path == "/ping_error") 
+		{
+			// Request count is reset every 15 mins
+			REQUESTS.first = 0;
+			REQUESTS.second = chrono::system_clock::now();
+
 			ltf("PING ERROR OCCURED: ", JsonMessage.GetParseError());
 		}
+
 		else {
 			request_handler(req, res);
 		}
 	});
 
-	svr.Get(".*", [](const Request& req, Response& res) {
-		if (req.path == "/status/status") {
-			res.set_content(getLBConfigAsJson(), "text/json");
+	svr.Get(".*", [](const Request& req, Response& res) 
+	{
+		//thread(notify_via_email, "ASLB TEST EMAIL!", "CHAL GYA! RADHA KANHA.").detach();
+
+			
+
+		#ifdef DEV_MODE
+		if (req.path == "/favicon.ico") {
+			res.set_content("404", "text/plain");
+			return;
 		}
-		else if (req.path == "/logs/logs") {
-			// logs to be implemented
+		#endif																					// DEV_MODE
+
+		if (req.path == "/status/status") {														// for getting aslb status
+			res.set_content(getLBConfigAsJson(REQUESTS.first, timePointToString(REQUESTS.second)), "text/json");
+			return;
 		}
+
+		else if (req.path == "/logs/logs")														// for getting log file content
+		{
+			ifstream config_file("aslb.log");
+			if (!config_file.is_open()) {
+				ltf("Unable to open `aslb_config.json`");
+				res.set_content("UNABLE TO LOAD LOG FILE !", "text/plain");
+				return;
+			}
+
+			string json((istreambuf_iterator<char>(config_file)), istreambuf_iterator<char>());
+			res.set_content(json.c_str(), "text/plain");
+			return;
+		}
+
 		else {
 			request_handler(req, res);
+			return;
 		}
 	});
 
@@ -186,7 +257,7 @@ int main(void) {
 
 	//ltf("SERVER STARTED ON PORT: [4000]");
 	cout << "SERVER STARTED ON PORT: [4000]" << endl;
-	svr.listen("2401:4900:1c97:b0f:862:1863:aa28:4766", 4000);
+	svr.listen("localhost", 4000);
 
 	return 0;
 }
